ee4ae1cc6022398105a5f47258b296cf87d5e888
tianhaining <tianhaining@miaozhen.com>
2018-02-12 16:21:10 +0800

    删除多余的模块

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var history = require('connect-history-api-fallback')
 
 //允许跨域
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "*")//第二个参数填写允许跨域的域名称，例如："http://a.com, http://b.com"，不建议直接写"*"，最好加个白名单限制
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
     res.header("Access-Control-Allow-Headers", "X-Requested-With")
     res.header('Access-Control-Allow-Headers', 'Content-Type')
